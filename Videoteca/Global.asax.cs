f7d1bd845ddef018d89fb220d47a41ff4e22a0f0
Eduardo Cobuci <educobuci@gmail.com>
2011-04-27 11:35:26 -0300

    remocao dos controllers e views

--- Videoteca/Global.asax.cs
+++ Videoteca/Global.asax.cs
@@ -18,12 +18,6 @@ namespace Videoteca
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute("edit_movie_path", "movies/edit/{id}", new { controller = "Movies", action = "Edit" });
-            routes.MapRoute("update_movie_path", "movies/update/{id}", new { controller = "Movies", action = "Update" });
-            routes.MapRoute("rate_movie_path", "movies/rate/{id}", new { controller = "Movies", action = "Rate" });
-
-            routes.MapRoute("genre_path", "genre/{id}", new { controller = "Genres", action = "show" });
-
             routes.MapRoute(
                 "Default", // Route name
                 "{controller}/{action}/{id}", // URL with parameters
--- Videoteca/Views/Home/Index.aspx
+++ Videoteca/Views/Home/Index.aspx
@@ -2,13 +2,4 @@
 <asp:Content ID="title" ContentPlaceHolderID="TitleContent" runat="server">Index</asp:Content>
 
 <asp:Content ID="content" ContentPlaceHolderID="MainContent" runat="server">
-<div id="movie-list">
-    <ul>
-        <% foreach (var movie in (this.ViewData["movies"] as IEnumerable<Movie>)) { %>
-        <li>
-            <% Html.RenderPartial("MovieBox", movie); %>
-        </li>
-        <% } %>
-    </ul>
-</div>
 </asp:Content>
